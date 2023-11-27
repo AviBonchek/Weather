8b9a57bd9968f2a63f10fabcdd894379a9360e31
AviBonchek <avibonch@gmail.com>
2023-11-27 15:02:12 +0200

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    let date = new Date();
+    return (
+      <header>
+        <div className="date">
+            <h1>
+                {date.toLocaleDateString()}
+            </h1>
+        </div>
+        <div className="webName">
+        <h1>Local weather</h1>
+        </div>
+      </header>
+    );
+  }
+  export default Header;
